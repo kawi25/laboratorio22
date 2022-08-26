60e8f4dfe71f185ede6595d3a955c534148f3435
FRANCISCO GABRIEL CHICAS BONILLA <SMSS117822@ugb.edu.sv>
2022-08-26 16:33:14 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace laboratorio22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] array1 = new int[5];
+            array1[2] = 8;
+            array1[0] = 15;
+            for (int i = 0; i < array1.Length; i++)
+            {
+                Console.WriteLine(i + " " + array1[i]);
+            }
+            for (int i = 0; i < array1.Length; i++)
+            {
+                array1[i] = i * 4;
+            }
+            foreach (int row in array1)
+            {
+                Console.WriteLine(row);
+            }
+            string[] paises = new string[5] { "El Salvador", "Mexico", "Chile", "Colombia", "Venezuela" };
+            foreach (string pais in paises)
+            {
+                Console.WriteLine(pais);
+            }
+        }
+    }
+}
